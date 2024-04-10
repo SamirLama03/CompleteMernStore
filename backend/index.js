cc557589395571ac44f865fc8ee5af9e8c86cbd0
SamirLama03 <117245671+SamirLama03@users.noreply.github.com>
2024-04-10 18:43:47 +0545

    fixing images

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,12 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
-// Utiles
-import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -23,18 +21,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Serve uploaded files statically
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Define API routes
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/orders", orderRoutes);
 
-// Serve uploaded files statically
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
+// Route to get PayPal client ID
 app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
+// Start the server
 app.listen(port, () => console.log(`Server running on port: ${port}`));
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "http://localhost:5000", // Change the target to port 5173
       "/uploads": {
         target: "http://localhost:5000",
         changeOrigin: true,
